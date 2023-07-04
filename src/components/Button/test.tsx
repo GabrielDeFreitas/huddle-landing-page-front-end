8696949e186e229d964c05d1ecf340339d23c8c8
gabriel freitas <gabrielfreitas.ilustra@gmail.com>
2023-07-04 16:12:32 -0300

    test: add button test

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -1,13 +1,67 @@
-import { render, screen } from '@testing-library/react'
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import Button from 'components/Button'
 
-import Button from '.'
+describe('Button component', () => {
+  it('renders button correctly', () => {
+    const { getByText } = render(<Button>Click me</Button>)
+    const buttonElement = getByText('Click me')
+    expect(buttonElement).toBeInTheDocument()
+  })
 
-describe('<Button />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Button />)
+  it('calls onClick function when clicked', () => {
+    const onClickMock = jest.fn()
+    const { getByText } = render(
+      <Button onClick={onClickMock}>Click me</Button>
+    )
+    const buttonElement = getByText('Click me')
+    fireEvent.click(buttonElement)
+    expect(onClickMock).toHaveBeenCalled()
+  })
 
-    expect(screen.getByRole('heading', { name: /Button/i })).toBeInTheDocument()
+  it('renders with primary variant by default', () => {
+    const { getByText } = render(<Button>Click me</Button>)
+    const buttonElement = getByText('Click me')
+    expect(buttonElement).toHaveClass('primary')
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  it('renders with secondary variant when specified', () => {
+    const { getByText } = render(<Button variant="secondary">Click me</Button>)
+    const buttonElement = getByText('Click me')
+    expect(buttonElement).toHaveClass('secondary')
+  })
+
+  it('renders disabled button when disabled prop is true', () => {
+    const { getByText } = render(<Button disabled>Click me</Button>)
+    const buttonElement = getByText('Click me')
+    expect(buttonElement).toBeDisabled()
+  })
+
+  it('renders enabled button when enabled prop is true', () => {
+    const { getByText } = render(<Button enabled>Click me</Button>)
+    const buttonElement = getByText('Click me')
+    expect(buttonElement).toBeEnabled()
+  })
+
+  it('renders button with specified size', () => {
+    const { getByText } = render(<Button size="lg">Click me</Button>)
+    const buttonElement = getByText('Click me')
+    expect(buttonElement).toHaveClass('lg')
+  })
+
+  it('renders button with specified aria attributes', () => {
+    const { getByText } = render(
+      <Button
+        ariaLabel="Aria Label"
+        ariaPressed
+        ariaDescribedby="description-id"
+      >
+        Click me
+      </Button>
+    )
+    const buttonElement = getByText('Click me')
+    expect(buttonElement).toHaveAttribute('aria-label', 'Aria Label')
+    expect(buttonElement).toHaveAttribute('aria-pressed', 'true')
+    expect(buttonElement).toHaveAttribute('aria-describedby', 'description-id')
   })
 })
